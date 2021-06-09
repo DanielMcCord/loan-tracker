20a0911526e2064f8c869b831041bc264cc4c398
Daniel McCord <git@danielmccord.dev>
2021-06-08 19:23:27 -0700

    Improve DBTable.

--- dbtable.h
+++ dbtable.h
@@ -8,7 +8,7 @@
 
 /**
  * A model of some of the features of a table in a relational database.
- * Will auto-increment if record_t inherits from AutoIncrementable.
+ * Will auto-increment the primary key if the records inherit from AutoIncrementable.
  */
 template <typename RecordT> class DBTable
 {
@@ -18,14 +18,27 @@ template <typename RecordT> class DBTable
     typedef std::map<key_t, RecordT> map_t;
     typedef typename DBTable<RecordT>::map_t::size_type size_type;
     DBTable();
-    bool add(RecordT &record);
+    /**
+     * @brief Adds a record to the table.
+     * @param record
+     * @param allowRecycledKey (optional) Whether, in the case of auto-incrementing primary keys,
+     * previously-removed keys may be used. Only has an effect if keys auto-increment.
+     * @return Whether the action was successful.
+     */
+    bool add(RecordT &record, bool allowRecycledKey = true);
     RecordT fetch(key_t primaryKey);
+    /**
+     * @brief Removes a record from the table.
+     * @param primaryKey
+     * @return
+     */
     size_type remove(key_t primaryKey);
 
   private:
     map_t records;
     /**
      * Used with auto-incrementable records only. Contains the value of the next key.
+     * If records are not auto-incrementable, this value sits unused and is never incrmeneted.
      */
     size_type nextKey;
     /**
@@ -34,26 +47,27 @@ template <typename RecordT> class DBTable
     std::queue<key_t> removedKeys;
 };
 
-template <typename RecordT> DBTable<RecordT>::DBTable() : nextKey(0)
+template <typename RecordT>
+DBTable<RecordT>::DBTable() : nextKey(0)
 {
 }
 
-template <typename RecordT> bool DBTable<RecordT>::add(RecordT &record)
+template <typename RecordT> bool DBTable<RecordT>::add(RecordT &record, bool allowRecycledKey)
 {
     // Only use auto-incrementing logic on records that are AutoIncrementable
     if constexpr (std::is_base_of_v<AutoIncrementable, RecordT>)
     {
-        // Recycling keys prevents unnecessary incrementing of nextKey.
-        bool usingRecycledKey = !removedKeys.empty();
+        // Prefers filling in gaps over making never-used keys.
+        bool usingRecycledKey = allowRecycledKey && !removedKeys.empty();
         size_type primaryKey = usingRecycledKey ? removedKeys.front() : nextKey;
         bool recordAdded = records.try_emplace(primaryKey, record).second;
 
         if (recordAdded)
         {
-            // Let the record know what key was assigned to it.
+            // Update the record so it knows what its own key is.
             record.primaryKey = primaryKey;
 
-            // Move on to the next key, whereever it came from.
+            // Make sure we don't try to use the same key next time.
             if (usingRecycledKey)
             {
                 removedKeys.pop();
@@ -79,18 +93,17 @@ template <typename RecordT> RecordT DBTable<RecordT>::fetch(key_t primaryKey)
     return records.at(primaryKey);
 }
 
-/**
- * Remove a record from the table.
- */
 template <typename RecordT>
 typename DBTable<RecordT>::size_type DBTable<RecordT>::remove(key_t primaryKey)
 {
+    // As with add(), only use auto-incrementing logic on records that are AutoIncrementable.
     if constexpr (std::is_base_of_v<AutoIncrementable, RecordT>)
     {
         size_type recordsRemoved = records.erase(primaryKey);
 
         if (recordsRemoved > 0)
         {
+            // Track the fact that the key is available again.
             removedKeys.push(primaryKey);
         }
 
