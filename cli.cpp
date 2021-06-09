333731c748b306ede48be48de40b05897b1c21a5
Daniel McCord <git@danielmccord.dev>
2021-06-08 20:34:47 -0700

    Add first pieces to CLI.

--- cli.cpp
+++ cli.cpp
@@ -1,5 +1,46 @@
 #include "cli.h"
 
+using namespace std;
+
 CLI::CLI()
+    : validCommands({"create item", "create loan", "list items", "list loans", "delete item",
+                     "save", "exit", "help"})
 {
+}
+
+CLI::CLI(const vector<string> &validCommands)
+{
+    this->validCommands = validCommands;
+}
+
+void CLI::welcome() const
+{
+    cout << "LoanIt - an item lending tracker." << endl;
+    cout << "For help, type 'help' (without quotes) and press enter." << endl;
+    list_valid_commands();
+}
+
+void CLI::on_invalid_command(const string &command) const
+{
+    cout << "Unrecognized command '" << command << "'." << endl;
+    list_valid_commands();
+}
+
+void CLI::list_valid_commands() const
+{
+    cout << "The following commands are supported:" << endl;
+
+    for (const string &command : validCommands)
+    {
+        cout << command << endl;
+    }
+}
+
+string CLI::prompt(const string &message) const
+{
+    cout << message << flush;
+    string input;
+    // Using std::ws to discard whitespace.
+    getline(cin >> ws, input);
+    return input;
 }
--- cli.h
+++ cli.h
@@ -2,11 +2,23 @@
 #define CLI_H
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 class CLI
 {
   public:
     CLI();
+    CLI(const std::vector<std::string> &validCommands);
+    void welcome() const;
+    /**
+     * @brief Displays a message when an invalid command is used.
+     * @param command The command that was used.
+     */
+    void on_invalid_command(const std::string &command) const;
+    void list_valid_commands() const;
+    std::string prompt(const std::string &message = "") const;
+    std::vector<std::string> validCommands;
 };
 
 #endif // CLI_H
